9493838a052e652cb865c241c2faccb196065b14
Avanindra Tiwari <avanindratiwari9@gmail.com>
2025-10-16 22:01:28 +0530

    Delete portfolio.js

--- portfolio.js
+++
@@ -1,8 +0,0 @@
-// Scroll to section smoothly (optional)
-document.querySelectorAll('nav a').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href'))
-      .scrollIntoView({ behavior: 'smooth' });
-  });
-});
